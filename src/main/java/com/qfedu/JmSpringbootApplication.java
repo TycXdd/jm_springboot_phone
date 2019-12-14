484d40baecc70c823aa1dad82f7120df008e6cef
TycXdd <1336445950@qq.com>
2019-12-14 14:48:41 +0800

    首次上传Demo1

--- src/main/java/com/qfedu/JmSpringbootApplication.java
+++ src/main/java/com/qfedu/JmSpringbootApplication.java
@@ -1,9 +1,11 @@
 package com.qfedu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qfedu.phone.dao")
 public class JmSpringbootApplication {
 
     public static void main(String[] args) {
